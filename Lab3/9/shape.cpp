7cb70f98f0edab5e5af05991a38497cabd5956da
_VXD7 <youmukonpaku@excite.co.jp>
2016-10-16 21:58:47 +0300

    WIP: Defined new functions for sphere and pyramid, bugfixes

--- Lab3/9/shape.cpp
+++ Lab3/9/shape.cpp
@@ -1,5 +1,34 @@
 #include "shape.h"
 
+spoint shape::rotate_point(spoint& sp, double xrot, double yrot, double zrot)
+{
+	myMatrix rotX, rotY, rotZ;
+	myMatrix centerVecMatrix;
+
+	centerVecMatrix.makeMatrixFromVector(sp.getRadiusVector());
+
+	rotX.makeRotMatrix("X", xrot);
+	rotY.makeRotMatrix("Y", yrot);
+	rotZ.makeRotMatrix("Z", zrot);
+
+	myMatrix res = rotX * centerVecMatrix;
+	res = rotY * res;
+	res = rotZ * res;
+
+	spoint resPoint;
+	resPoint.x = res.matrix[0][0];
+	resPoint.y = res.matrix[1][0];
+	resPoint.z = res.matrix[2][0];
+	
+	return resPoint;
+}
+
+spoint::spoint()
+{
+	x = 0.0;
+	y = 0.0;
+	z = 0.0;
+}
 spoint::spoint(double newx, double newy, double newz)
 {
 	x = newx;
@@ -7,12 +36,12 @@ spoint::spoint(double newx, double newy, double newz)
 	z = newz;
 }
 
-void spoint::operator=(spoint &ptn1)
-{
-	x = ptn1.x;
-	y = ptn1.y;
-	z = ptn1.z;
-}
+//void spoint::operator=(spoint &ptn1)
+//{
+//	x = ptn1.x;
+//	y = ptn1.y;
+//	z = ptn1.z;
+//}
 myvector spoint::getRadiusVector()
 {
 	return myvector(x, y, z);
@@ -24,10 +53,22 @@ void spoint::fromVector(myvector vec)
 	y = vec.getVectorCoords().gety();
 	z = vec.getVectorCoords().getz();
 }
+void spoint::setAllCoords(double newX, double newY, double newZ)
+{
+	x = newX;
+	y = newY;
+	z = newZ;
+}
 
 /*
  * ----- Sphere -----
  */
+
+
+sphere::sphere()
+{
+	
+}
 
 /**
  * Make a generic sphere,
@@ -52,24 +93,96 @@ void sphere::moveShape(myvector moveVector)
 
 void sphere::rotateShape(double xrot, double yrot, double zrot)
 {
-	myMatrix rotX, rotY, rotZ;
-	myMatrix centerVecMatrix;
-
-	centerVecMatrix.makeMatrixFromVector(ptCenter.getRadiusVector());
-
-	rotX.makeRotMatrix("X", xrot);
-	rotY.makeRotMatrix("Y", yrot);
-	rotZ.makeRotMatrix("Z", zrot);
-
-	myMatrix res = rotX * rotY * rotZ * centerVecMatrix;
-
-	ptCenter.x = res.matrix[0][0];
-	ptCenter.y = res.matrix[1][0];
-	ptCenter.z = res.matrix[2][0];
+	ptCenter = rotate_point(ptCenter, xrot, yrot, zrot);
 	
 }
 
+void sphere::printShape()
+{
+	std::cout << "Sphere" << std::endl;
+	std::cout << "Center: (" << ptCenter.x << ", " << ptCenter.y << ", " << ptCenter.z << ")" << std::endl;
+	std::cout << "Radius: " << rad << std::endl;
+}
 /**
  * ----- Pyramid -----
  */
 
+pyramid::pyramid()
+{
+	
+}
+
+/**
+ * Make a generic pyramid
+ * Square base
+ */
+void pyramid::mkShape()
+{
+	spoint bp[5];
+	bp[1].setAllCoords(1.0, 0.0, 0.0);
+	bp[2].setAllCoords(0.0, 0.0, 1.0);
+	bp[3].setAllCoords(-1.0, 0.0, 0.0);
+	bp[4].setAllCoords(0.0, 0.0, -1.0);
+
+	basepoints.push_back(bp[1]);
+	basepoints.push_back(bp[2]);
+	basepoints.push_back(bp[3]);
+	basepoints.push_back(bp[4]);
+
+	ptTop.setAllCoords(0.0, 1.0, 0.0);
+	
+}
+void pyramid::mkShape(spoint top, std::vector <spoint> basePts)
+{
+	if(basePts.size() < 3) {
+		std::cerr << "Error! Pyramid can't have less than 3 basepoints!";
+	}
+
+	basepoints = basePts;
+	ptTop = top;
+}
+void pyramid::moveShape(myvector moveVector)
+{
+	/**
+	 * Move all the basepoints
+	 */
+	for(size_t i = 0; i < basepoints.size(); ++i) {
+		basepoints[i].fromVector(basepoints[i].getRadiusVector() + moveVector);
+	}
+
+	/**
+	 * Move the top
+	 */
+	ptTop.fromVector(ptTop.getRadiusVector() + moveVector);
+	
+}
+void pyramid::rotateShape(double xrot, double yrot, double zrot)
+{
+	/**
+	 * Rotate all the points of the base
+	 */
+	for(size_t i = 0; i < basepoints.size(); ++i) {
+		basepoints[i] = rotate_point(basepoints[i], xrot, yrot, zrot);
+	}
+
+	/**
+	 * Rotate the top point
+	 */
+	ptTop = rotate_point(ptTop, xrot, yrot, zrot);
+	
+}
+
+void pyramid::printShape()
+{
+	std::cout << "Pyramid: " << std::endl;
+	std::cout << "basepoints:" << std::endl;
+	std::cout << "-----------" << std::endl;
+
+	for(size_t i = 0; i < basepoints.size(); ++i) {
+
+		std::cout << "Point " << i << ": " << "(" << basepoints[i].x << ", " << basepoints[i].y << ", " << 
+			basepoints[i].z << ")" << std::endl;
+	}
+	std::cout << "-----------" << std::endl;
+	std::cout << "Top: " << "(" << ptTop.x << ", " << ptTop.y << ", " << ptTop.z << ")" << std::endl;
+}
--- Lab3/9/shape.h
+++ Lab3/9/shape.h
@@ -8,41 +8,46 @@
 
 class spoint {
 public:
+	spoint();
 	spoint(double newx, double newy, double newz);
 
 	double x, y, z;
 
-	void operator=(spoint &ptn1);
+	//void operator=(spoint &ptn1);
 	myvector getRadiusVector();
 	void fromVector(myvector vec);
+
+	void setAllCoords(double newX, double newY, double newZ);
 	
 };
 
 class shape {
 public:
-	shape();
-	~shape();
 
 	virtual void mkShape() = 0; /*< Pure virtual function -- makes an abstract class */
 	virtual void moveShape(myvector moveVector) = 0;
 	virtual void rotateShape(double xrot, double yrot, double zrot) = 0;
+	virtual void printShape() = 0;
 
 private:
 protected:
 	int numberOfVectices;
 	int numberOfFaces;
 	int numberOfEdges;
+
+	spoint rotate_point(spoint& sp, double xrot, double yrot, double zrot);
 };
 
 class sphere : public shape {
 public:
 	sphere();
-	~sphere();
 
 	void mkShape();
 	void mkShape(spoint center, double radius);
 	void moveShape(myvector moveVector);
 	void rotateShape(double xrot, double yrot, double zrot);
+
+	void printShape();
 private:
 	spoint ptCenter;
 	double rad;
@@ -51,26 +56,28 @@ private:
 class pyramid : public shape {
 public:
 	pyramid();
-	~pyramid();
 
 	void mkShape();
 	void mkShape(spoint top, std::vector <spoint> basePts);
 	void moveShape(myvector moveVector);
 	void rotateShape(double xrot, double yrot, double zrot);
+
+	void printShape();
 private:
 	spoint ptTop; /*<Top of the pyramid */
-	std::vector <spoint> basespoints;
+	std::vector <spoint> basepoints;
 };
 
 class parallelepiped : public shape {
 public:
 	parallelepiped();
-	~parallelepiped();
 
 	void mkShape();
 	void mkShape(std::vector <spoint> vertices);
 	void moveShape(myvector moveVector);
 	void rotateShape(double xrot, double yrot, double zrot);
+
+	void printShape();
 protected:
 	std::vector <spoint> ptVectices;
 	spoint ptCenter;
@@ -79,23 +86,25 @@ protected:
 class cube : public parallelepiped { /*< Cube is a three-dimensional solid object bounded by six square faces, with three meeting at each vertex */
 public:
 	cube();
-	~cube();
 
 	void mkShape();
 	void mkShape(std::vector <spoint> vertices);
 	void moveShape(myvector moveVector);
 	void rotateShape(double xrot, double yrot, double zrot);
+
+	void printShape();
 private:
 };
 
 class cuboid : public parallelepiped { /*< Just a simple rectangular box */
 public:
 	cuboid();
-	~cuboid();
 
 	void mkShape();
 	void mkShape(std::vector <spoint> vertices);
 	void moveShape(myvector moveVector);
 	void rotateShape(double xrot, double yrot, double zrot);
+
+	void printShape();
 private:
 };
